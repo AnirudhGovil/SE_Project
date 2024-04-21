5245114551480ab0d72b4447a72ec352abadcf45
vanhalen42 <gauravsinghk03@gmail.com>
2024-04-21 23:06:32 +0530

    generate course feedbacks

--- data_generation/generate_qualitative.py
+++ data_generation/generate_qualitative.py
@@ -83,6 +83,15 @@ def load_course_prompt(code, courses, course_features_aggregated):
     
     return prompt
 
+def parse_results(results):
+    results_lines = results.split("\n")
+    # print(results_lines)
+    feedbacks = []
+    for lines in results_lines:
+        if lines.startswith("Feedback Number"):
+            feedbacks.append(lines.split(": ")[-1])
+    assert len(feedbacks) == 10, "Feedbacks not generated properly"
+    return feedbacks
 
 def main(
     ckpt_dir: str,
@@ -104,11 +113,15 @@ def main(
 
     `max_gen_len` is optional because finetuned models are able to stop generations naturally.
     """
+    feedbacks = {}
     courses = load_courses()[:2]
     course_feat = load_course_features()
     prompts = []
+    course_codes = []
     for course in courses:
         course_code = course['ID']
+        feedbacks[course_code] = []
+        course_codes.append(course_code)
         prompt = load_course_prompt(course_code, courses, course_feat)
         prompts.append(prompt)
     # print(prompts)
@@ -122,42 +135,29 @@ def main(
     )
     
     dialogs: List[Dialog] = prompts
-    # dialogs: List[Dialog] = [
-    #     [{
-    #         "role": "user", 
-    #         "content": """
-    #         what is the general sentiment of this review? Note the ratings are out of 10 and the general bounds of a good course is > 7.
-            
-    #         "MA22053": {
-    #         "How would you rate the difficulty of the course?": 5,
-    #         "How would you rate the extent of time commitment required for the course?": 5,
-    #         "How much would you say you learned from the course?": 5,
-    #         "How well do you think the course was structured?": 5,
-    #         "How would you rate the quality of the course material?": 5,
-    #         "How would you rate the quality of the assignments?": 5,
-    #         "How would you rate the quality of the exams?": 5,
-    #         "How well did the course align with your expectations?": 4,
-    #         "How likely are you to recommend this course to your juniors?": 6,
-    #         "How well did the course cover topics such as logic, sets, functions, and combinatorics?": 4,
-    #         "To what extent did the course provide insights into applications of discrete mathematics in computer science and cryptography?": 3,
-    #         "How useful were the assignments and problem-solving exercises in understanding discrete mathematics concepts?": 7}
-    #         """}],
-    # ]
+
     results = generator.chat_completion(
         dialogs,
         max_gen_len=max_gen_len,
         temperature=temperature,
         top_p=top_p,
     )
-
+    i = 0
     for dialog, result in zip(dialogs, results):
-        for msg in dialog:
-            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-        print(
-            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
-        )
-        # print(result['generation']['content'])
-        print("\n==================================\n")
+        # for msg in dialog:
+        #     print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+        # print(
+        #     f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+        # )
+        # # print(result['generation']['content'])
+        feedback = parse_results(result['generation']['content'])
+        feedbacks[course_codes[i]] = feedback
+        i += 1
+        # print("\n==================================\n")
+    
+    with open('course_feedbacks.json', 'w') as file:
+        # dump the feedbacks to a json file with spaces and newlines
+        json.dump(feedbacks, file, indent=4)
 
 
 if __name__ == "__main__":
